c4e20f941dd11ed165884e0b59870d8d326fe330
Joey Beninghove <me@joey.io>
2019-12-05 16:34:59 -0500

    Use htmlentities to decode erb template
    This is to replace the ugly manual gsubbing I was doing before.

--- lib/svgthang/erb_converter.rb
+++ lib/svgthang/erb_converter.rb
@@ -1,4 +1,5 @@
 require "oga"
+require "htmlentities"
 
 module SvgThang
   class ErbConverter
@@ -12,7 +13,7 @@ module SvgThang
       svg_doc = Oga.parse_html(File.read(source_path))
       svg_doc.at_css("svg").set(
         "class", "#{default_classes} <%= defined?(classes) ? classes : nil %>")
-      html = svg_doc.to_xml.gsub("&lt;", "<").gsub("&gt;", ">")
+      html = HTMLEntities.new.decode(svg_doc.to_xml)
 
       filename = "#{File.basename(target_path, ".svg")}.html.erb"
       new_target_path = Pathname.new(target_path).parent.join(filename)
--- lib/svgthang/liquid_converter.rb
+++ lib/svgthang/liquid_converter.rb
@@ -12,7 +12,7 @@ module SvgThang
       svg_doc = Oga.parse_html(File.read(source_path))
       svg_doc.at_css("svg").set(
         "class", "#{default_classes} {{ include.classes }}")
-      html = svg_doc.to_xml.gsub("&lt;", "<").gsub("&gt;", ">")
+      html = svg_doc.to_xml
 
       File.open(target_path, "w") do |f|
         f.write(html)
